48af2f1827600cb4103d0170978f03a49d63cd00
MitoCode <mitocode@jaime.cpe.tdp.com>
2016-10-31 22:53:22 -0500

    Hasta tuto 2

---
+++ src/main/java/com/mitocode/java8/lambda/LambdaApp.java
@@ -0,0 +1,54 @@
+package com.mitocode.java8.lambda;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaApp {
+
+	public void ordernar() {
+		List<String> lista = new ArrayList<>();
+		lista.add("MitoCode");
+		lista.add("Code");
+		lista.add("Mito");
+
+		// JDK < 1.
+		/*
+		 * Collections.sort(lista, new Comparator<String>() {
+		 * 
+		 * @Override public int compare(String o1, String o2) { return
+		 * o1.compareTo(o2); }
+		 * 
+		 * });
+		 */
+
+		// Lambda
+		Collections.sort(lista, (String p1, String p2) -> p1.compareTo(p2));
+
+		for (String elemento : lista) {
+			System.out.println(elemento);
+		}
+	}
+
+	public void calcular() {
+		/*Operacion operacion = new Operacion() {
+			@Override
+			public double calcularPromedio(double n1, double n2) {
+				return (n1 + n2) / 2;
+			}
+		};
+
+		System.out.println(operacion.calcularPromedio(2, 3));*/
+		
+		Operacion operacion = (double x, double y) -> (x+y)/2;
+		System.out.println(operacion.calcularPromedio(2, 3));
+	}
+
+	public static void main(String[] args) {
+		LambdaApp app = new LambdaApp();
+		//app.ordernar();
+		app.calcular();
+	}
+
+}
---
+++ src/main/java/com/mitocode/java8/lambda/Operacion.java
@@ -0,0 +1,6 @@
+package com.mitocode.java8.lambda;
+
+public interface Operacion {
+
+	double calcularPromedio(double n1, double n2);
+}
